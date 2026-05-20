f523783eaf4f73c98951c61551a8b113e6199170
SaathvikBogam <saathvik183@gmail.com>
2026-05-20 15:03:44 +0530

    small chanage

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
-  site: "https://SaathvikBogam.github.io/Saathvik-s-Website",
+  site: "https://SaathvikBogam.github.io/swebsite",
   integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkMath],
